40fc2ba9b21a51ff65a577fb2e0dc78964f3a2ec
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2025-09-25 00:36:36 +0900

    Formatting konsulent-api

--- konsulent-api/main.py
+++ konsulent-api/main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
-from typing import List
 from data.consultants import CONSULTANTS_DATA
 
 app = FastAPI(title="Konsulent API", version="1.0.0")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello from Konsulent API"}
+  return {"message": "Hello from Konsulent API"}
 
 @app.get("/konsulenter")
-async def get_consultants() -> List[dict]:
-    """
-    Return a list of all consultants
-    """
-    return CONSULTANTS_DATA
+async def get_consultants() -> list[dict]:
+  """
+  Return a list of all consultants
+  """
+  return CONSULTANTS_DATA
